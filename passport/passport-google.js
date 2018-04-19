4e4b5581e7c8fa1d92c1811b23e92b3ef2275efc
unknown <SSuuribaatar@G752VT.kuikdelivery.com>
2018-04-20 05:40:08 +0800

    Edit Commit

--- passport/passport-google.js
+++ passport/passport-google.js
@@ -1,50 +1,50 @@
-'use strict';
+// 'use strict';
 
-const passport = require('passport');
-const User = require('../models/user');
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+// const passport = require('passport');
+// const User = require('../models/user');
+// const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
-passport.serializeUser((user, done) => {
-    done(null, user.id);
-});
+// passport.serializeUser((user, done) => {
+//     done(null, user.id);
+// });
 
-passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => {
-        done(err, user);
-    });
-});
+// passport.deserializeUser((id, done) => {
+//     User.findById(id, (err, user) => {
+//         done(err, user);
+//     });
+// });
 
-passport.use(new GoogleStrategy({
-    clientID: '',
-    clientSecret: '',
-    callbackURL: 'http://localhost:3000/auth/google/callback',
-    passReqToCallback: true
+// passport.use(new GoogleStrategy({
+//     clientID: '',
+//     clientSecret: '',
+//     callbackURL: 'http://localhost:3000/auth/google/callback',
+//     passReqToCallback: true
     
-}, (req, accessToken, refreshToken, profile, done) => {
-    User.findOne({google:profile.id}, (err, user) => {
-        if(err){
-           return done(err);
-        }
+// }, (req, accessToken, refreshToken, profile, done) => {
+//     User.findOne({google:profile.id}, (err, user) => {
+//         if(err){
+//            return done(err);
+//         }
         
-        if(user){
-            return done(null, user);
-        }else{
-            const newUser = new User();
-            newUser.google = profile.id;
-            newUser.fullname = profile.displayName;
-            newUser.username = profile.displayName;
-            newUser.email = profile.emails[0].value;
-            newUser.userImage = profile._json.image.url;
+//         if(user){
+//             return done(null, user);
+//         }else{
+//             const newUser = new User();
+//             newUser.google = profile.id;
+//             newUser.fullname = profile.displayName;
+//             newUser.username = profile.displayName;
+//             newUser.email = profile.emails[0].value;
+//             newUser.userImage = profile._json.image.url;
             
-            newUser.save((err) => {
-                if(err){
-                    return done(err)
-                }
-                return done(null, newUser);
-            })
-        }
-    })
-}));
+//             newUser.save((err) => {
+//                 if(err){
+//                     return done(err)
+//                 }
+//                 return done(null, newUser);
+//             })
+//         }
+//     })
+// }));
 
 
 
